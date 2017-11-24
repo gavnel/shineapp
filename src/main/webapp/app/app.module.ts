aa3dcbdd4ee91da6e7f6e5432518e40ae906a03f
Gaveen <gavneliah@gmail.com>
2017-11-24 14:54:28 +0400

    Contact, Solution and About us pages added

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,6 +14,9 @@ import { customHttpProvider } from './blocks/interceptor/http.provider';
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
 
 // jhipster-needle-angular-add-module-import JHipster will add new module here
+import { ShineSolutionsModule } from './solutions/solutions.module';
+import { ShineAboutUsModule } from './shine/shine.module';
+import { ShineContactModule } from './contact/contact.module';
 
 import {
     JhiMainComponent,
@@ -37,6 +40,9 @@ import {
         ShineAccountModule,
         ShineEntityModule,
         // jhipster-needle-angular-add-module JHipster will add new module here
+        ShineSolutionsModule,
+        ShineAboutUsModule,
+        ShineContactModule
     ],
     declarations: [
         JhiMainComponent,
